7b2e08cc8b768591a3b6749113b192131c0c5e59
Brad Walls <walls.200@buckeyemail.osu.edu>
2021-02-14 17:55:58 -0800

    continued work on game

--- GameEngine.rb
+++ GameEngine.rb
@@ -9,14 +9,26 @@ played_deck = []
 deck_on_table = []
 cards_on_table = 0
 cards_in_deck = 0
+output_dashes = ""
+output_first_line = ""
+output_second_line = ""
+output_third_line = ""
+
 puts "Welcome to the game of Set! \n"
 puts "To quit at any time, type quit\n"
 puts "The game is played 12 cards at a time. The goal is to make a set of three cards that have nothing in common with one-another!\n"
 puts "Every card is given a number: 1-12, to make a set, enter the three numbers separated by spaces!\n"
-puts "For example, if cards 3, 5, and 10 are a set, enter: 3 5 10 or 5 10 3 or 10 3 5"
+puts "For example, if cards 3, 5, and 10 are a set, enter: 3 5 10 or 5 10 3 or 10 3 5\n"
 
 def print_deck(on_screen_deck) # Take the on-screen deck and print it
+  on_screen_deck.each do |card|
+    line_builder(card)
+  end
+  puts output_string + "\n"
+  on_screen_deck.length.times do |i|
+    output_meat
 
+def line_builder(card)
 
 
 while keep_running do
