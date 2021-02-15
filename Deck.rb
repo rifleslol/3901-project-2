dd6b72a7d9071ab3634012b9a012412324bcf9f4
Brad Walls <walls.200@buckeyemail.osu.edu>
2021-02-14 16:10:45 -0800

    general structure changes

--- Deck.rb
+++ Deck.rb
@@ -3,27 +3,27 @@ require_relative 'SetCard'
 
 class Deck
   def initialize
+    @Deck = []
     @shapes = ["diamond", "squiggle", "oval"]
     @shadings = ["solid", "stripe", "open"]
     @colors = ["red", "green", "blue"]
     @numbers = ["one", "two", "three"]
 
-    def self.create_deck
-      @shapes map do |shape|
-        @shadings.map do |shade|
-          @colors.map do |color|
-            @numbers.map do |number|
-              {
-                SetCard.new(shape, shade, color, number)
-              }
-            end
+    @shapes.each do |shape|
+      @shadings.each do |shade|
+        @colors.each do |color|
+          @numbers.each do |number|
+            {
+              new_card = SetCard.new(shape, shade, color, number)
+              temp_card = new_card.create_card()
+              @Deck << temp_card
+            }
           end
         end
       end
     end
 
-    SetDeck = create_deck
-
-    def triples
-      SetDeck.combination(3).to_a
+    def print_deck
+      print @Deck
     end
+end
--- SetCard.rb
+++ SetCard.rb
@@ -1,40 +1,12 @@
 class SetCard
-  include Comparable
 
-  CARD_ATTRIBUTES = {
-    :number => ["one", "two", "three"]
-    :color => {"red", "green", "blue"}
-    :shade => {"solid", "stripe", "open"}
-    :shape => {"diamond", "squiggle", "oval"}
-  }
+  def initialize(*attributes)
+    @card_shape = properties[0]
+    @card_shade = properties[1]
+    @card_color = properties[2]
+    @card_number = properties[3]
+  end
 
-def self.attribute_keys
-  CARD_ATTRIBUTES.keys
-end
-
-def self.number
-  CARD_ATTRIBUTES[:number]
-end
-
-def self.color
-  CARD_ATTRIBUTES[:color]
-end
-
-def self.shade
-  CARD_ATTRIBUTES[:shade]
-end
-
-def self.shape
-  CARD_ATTRIBUTES[:shape]
-end
-
-def self.attributes_setter (shapeIn, shadeIn, colorIn, numberIn)
-  {
-  shape: shapeIn, shade: shadeIn, color: colorIn, number: numberIn
-  }
-end
-
-def self.create_card()
-  {
-    Card.new(attributes_setter(shapeIn, shadeIn, colorIn, numberIn))
-  }
+  def make_card
+    card = [@card_shape, @card@shade, @card_color, @card_number]
+  end
